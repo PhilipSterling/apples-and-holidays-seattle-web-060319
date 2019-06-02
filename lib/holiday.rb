6ddfc797fc6bb5c765f9d3635ba0e5b70e53034b
Philip Sterling <philipssterlings@gmail.com>
2019-06-02 10:25:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts key.to_s.capitalize! + ":"
     value.each do |key2, value2|
       string = value2.join(", ")
-      puts key2.to_s.capitalize! + ": " + string.capitalize!
+      puts key2.to_s.capitalize! + ": " + string.titleize!
     end
   end
 end
